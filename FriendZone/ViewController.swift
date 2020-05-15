311f674358a0a7fddb36d0af5a485555090c0787
user162638 <mrajago@purdue.edu>
2020-05-15 19:22:45 -0400

    Initial Commit

---
+++ FriendZone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendZone
+//
+//  Created by user162638 on 5/15/20.
+//  Copyright © 2020 user162638. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
